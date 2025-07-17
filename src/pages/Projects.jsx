cbfb352bab2e1d8a09a29780262f7d6827f16146
Andrew Martinez <andy72391@gmail.com>
2025-07-17 06:31:58 -0400

    Changing the projects section structure and adding some of my accomplished projects.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,67 +1,158 @@
 export default function Projects() {
   return (
     <section className="projects">
-      <h1 className="project-title">My Projects</h1>
-
-      <div className="project-container">
+      <div className="project-card">
         <div className="project-description">
-          <h2>Project Overview</h2>
+          <h2>Shifts Logger Console App</h2>
           <p>
-            This project is built with React and demonstrates various web
-            development techniques such as creating reusable components,
-            managing state, and styling using CSS.
+            This is a console app using both .NET and ASP.NET. The application
+            allows users to log their shifts and view their shift history. It
+            uses Entity Framework for database management and SQL Server for
+            data storage.
           </p>
         </div>
-
-        <div className="project-features">
-          <h2>Features</h2>
-          <ul className="feature-list">
-            <li className="feature-list-item">Responsive layout</li>
-            <li className="feature-list-item">Reusable components</li>
-            <li className="feature-list-item">Styled with custom CSS</li>
-            <li className="feature-list-item">State management</li>
-          </ul>
+        <div className="project-link">
+          <a href="https://github.com/andymartinez1/Shifts-Logger-App">
+            View on GitHub
+          </a>
+        </div>
+        <div className="project-tags">
+          <span>C#</span>
+          <span>Entity Framework</span>
+          <span>SQL Server</span>
+        </div>
+        <div className="project-image">
+          <img src="src/assets/img/projects/Shifts-Logger.webp" alt="Project" />
         </div>
       </div>
 
-      <div className="project-container">
-        <div className="project-features">
-          <h2>Features</h2>
-          <ul className="feature-list">
-            <li className="feature-list-item">Responsive layout</li>
-            <li className="feature-list-item">Reusable components</li>
-            <li className="feature-list-item">Styled with custom CSS</li>
-            <li className="feature-list-item">State management</li>
-          </ul>
+      <div className="project-card">
+        <div className="project-image">
+          <img
+            src="src/assets/img/projects/Under-Construction.webp"
+            alt="Project"
+          />
         </div>
         <div className="project-description">
           <h2>Project Overview</h2>
           <p>
-            This project is built with React and demonstrates various web
-            development techniques such as creating reusable components,
-            managing state, and styling using CSS.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusantium ad labore, ipsum dicta soluta molestiae harum facere nam
+            tempora tempore, deleniti exercitationem animi deserunt, cumque
+            dolor officiis rem? Nostrum, cum.
           </p>
+        </div>
+        <div className="project-link">
+          <a href="#"> View on GitHub</a>
+        </div>
+        <div className="project-tags">
+          <span>Test</span>
+          <span>Test</span>
+          <span>Test</span>
         </div>
       </div>
 
-      <div className="project-container">
+      <div className="project-card">
         <div className="project-description">
           <h2>Project Overview</h2>
           <p>
-            This project is built with React and demonstrates various web
-            development techniques such as creating reusable components,
-            managing state, and styling using CSS.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusantium ad labore, ipsum dicta soluta molestiae harum facere nam
+            tempora tempore, deleniti exercitationem animi deserunt, cumque
+            dolor officiis rem? Nostrum, cum.
           </p>
         </div>
+        <div className="project-link">
+          <a href="#"> View on GitHub</a>
+        </div>
+        <div className="project-tags">
+          <span>Test</span>
+          <span>Test</span>
+          <span>Test</span>
+        </div>
+        <div className="project-image">
+          <img
+            src="src/assets/img/projects/Under-Construction.webp"
+            alt="Project"
+          />
+        </div>
+      </div>
 
-        <div className="project-features">
-          <h2>Features</h2>
-          <ul className="feature-list">
-            <li className="feature-list-item">Responsive layout</li>
-            <li className="feature-list-item">Reusable components</li>
-            <li className="feature-list-item">Styled with custom CSS</li>
-            <li className="feature-list-item">State management</li>
-          </ul>
+      <div className="project-card">
+        <div className="project-image">
+          <img
+            src="src/assets/img/projects/Under-Construction.webp"
+            alt="Project"
+          />
+        </div>
+        <div className="project-description">
+          <h2>Project Overview</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusantium ad labore, ipsum dicta soluta molestiae harum facere nam
+            tempora tempore, deleniti exercitationem animi deserunt, cumque
+            dolor officiis rem? Nostrum, cum.
+          </p>
+        </div>
+        <div className="project-link">
+          <a href="#"> View on GitHub</a>
+        </div>
+        <div className="project-tags">
+          <span>Test</span>
+          <span>Test</span>
+          <span>Test</span>
+        </div>
+      </div>
+
+      <div className="project-card">
+        <div className="project-description">
+          <h2>Project Overview</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusantium ad labore, ipsum dicta soluta molestiae harum facere nam
+            tempora tempore, deleniti exercitationem animi deserunt, cumque
+            dolor officiis rem? Nostrum, cum.
+          </p>
+        </div>
+        <div className="project-link">
+          <a href="#"> View on GitHub</a>
+        </div>
+        <div className="project-tags">
+          <span>Test</span>
+          <span>Test</span>
+          <span>Test</span>
+        </div>
+        <div className="project-image">
+          <img
+            src="src/assets/img/projects/Under-Construction.webp"
+            alt="Project"
+          />
+        </div>
+      </div>
+
+      <div className="project-card">
+        <div className="project-image">
+          <img
+            src="src/assets/img/projects/Under-Construction.webp"
+            alt="Project"
+          />
+        </div>
+        <div className="project-description">
+          <h2>Project Overview</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusantium ad labore, ipsum dicta soluta molestiae harum facere nam
+            tempora tempore, deleniti exercitationem animi deserunt, cumque
+            dolor officiis rem? Nostrum, cum.
+          </p>
+        </div>
+        <div className="project-link">
+          <a href="#"> View on GitHub</a>
+        </div>
+        <div className="project-tags">
+          <span>Test</span>
+          <span>Test</span>
+          <span>Test</span>
         </div>
       </div>
     </section>
